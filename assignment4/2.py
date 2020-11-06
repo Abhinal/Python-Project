2928d93ce188ade355674171f7c26f24a87df867
Abhinav Agarwal <abhinavagarwalsps@gmail.com>
2020-11-06 12:46:48 +0530

    Abhinav Agarwal
    Signed-off-by: Abhinav Agarwal <abhinavagarwalsps@gmail.com>

--- assignment4/2.py
+++ assignment4/2.py
@@ -1,8 +1,11 @@
-def changeSentence(sentence, word):
-    reverseWord = word[::-1]
-    sentence = sentence.replace(word,reverseWord)
-    return sentence
+def reverseWord(word):
+    return word[::-1]
+
+def sliceSentence(sentence):
+    words = sentence.split(" ")
+    for word in words:
+        print(reverseWord(word),end=" ")
+    print('\n')
 
 sentence = input('Enter a sentence : ')
-word = input('Enter a word : ')
-print(changeSentence(sentence, word))
+sliceSentence(sentence)
